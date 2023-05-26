97ca0f300835c00800f8c4e722f795f34043e155
Che Jin <robin912104@gmail.com>
2023-05-26 11:52:48 +0800

    Refactor codes

--- utils/helper_functions.js
+++ utils/helper_functions.js
@@ -1,25 +1,5 @@
 const { ethers } = require('hardhat');
 
-const sendEth = async (account) => {
-    await hre.network.provider.request({
-        method: "hardhat_setBalance",
-        params: [account, ethers.utils.parseEther('15').toHexString()]
-    });
-}
-
-const impersonate = async (account) => {
-    await hre.network.provider.request({
-        method: "hardhat_impersonateAccount",
-        params: [account]
-    });
-    return await ethers.getSigner(account);
-}
-
-const increaseTime = async (seconds) => {
-    await network.provider.send("evm_increaseTime", [seconds]);
-    await network.provider.send("evm_mine");
-}
-
 const toBN = (numb, exp) => {
     return ethers.utils.parseUnits(numb, exp);
 }
@@ -36,9 +16,6 @@ const Const = {
 }
 
 module.exports = {
-    sendEth,
-    impersonate,
-    increaseTime,
     toBN,
     Const
 }
